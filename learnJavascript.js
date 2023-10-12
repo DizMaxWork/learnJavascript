c4d315ae644eaa02b0c080b316d687791fec5500
DizMaxWork <sol.work@mail.ru>
2023-10-12 17:33:43 +0300

    Закончил день

--- learnJavascript.js
+++ learnJavascript.js
@@ -260,3 +260,43 @@
 
 // console.log(users.size);
 // users.forEach((user) => console.log(user));
+
+// Задачи на JSON
+
+// let user = {
+//   name: "Василий Иванович",
+//   age: 35,
+// };
+// user = JSON.stringify(user);
+// console.log(user);
+// let back = JSON.parse(user);
+// console.log(back);
+
+// let room = {
+//   number: 23,
+// };
+
+// let meetup = {
+//   title: "Совещание",
+//   occupiedBy: [{ name: "Иванов" }, { name: "Петров" }],
+//   place: room,
+// };
+
+// цикличные ссылки
+// room.occupiedBy = meetup;
+// meetup.self = meetup;
+
+// alert(
+//   JSON.stringify(meetup, function replacer(key, value) {
+//     return key != "" && value == meetup ? undefined : value;
+//   })
+// );
+
+// Деструктурирующее присваивание
+
+// let user = {
+//   name: "John",
+//   age: 30,
+// };
+// let { name, age, isAdmin = false } = user;
+// console.log(isAdmin);
